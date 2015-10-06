cc92834f26b230441706498dfb84d5ddd10b1471
Ben <ben@doginthehat.com.au>
2015-10-06 11:51:00 +1100

    Test gulp-header

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	header = require('gulp-header');
 	
 gulp.task('sass', function () {
   return gulp
     .src('build.scss')
 	.pipe(sass({outputStyle: 'compressed'}).on('error', function(err) {notify().write(err); this.emit('end');}))
+	.pipe(header('/*! Imma destroy your bom */'))
 	.pipe(gulp.dest('./build'));
 });
